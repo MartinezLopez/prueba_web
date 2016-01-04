1fb3267fc9a1652f56133e749fbbaf7cdc5b4d87
Miguel Angel Martinez Lopez <michel@iMac-de-Miguel.local>
2016-01-04 21:10:38 +0100

    port changed

--- app.py
+++ app.py
@@ -154,5 +154,5 @@ def dir_listing(req_path):
 
 
 if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 8080))
+	port = int(os.environ.get('PORT', 80))
 	app.run(debug=True, host='0.0.0.0', port=port)
